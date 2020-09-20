fe1b8e5e181e3afa5f9bc9b30b453d889f324281
Gustavo Maltez <gustavo.maltez@hotmail.com>
2020-09-20 01:41:30 -0300

    Added main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/account/infos', (req,res) => {
+    
+    //Deconstruct access token
+    const { token } = req.body;
+
+    //Verify if exists a token in body request
+    if(!token) res.status(400).json({"error": "No token identified"});
+
+    // ...
+    return res.json({"example": 123})
+})
+
+app.listen(3000);

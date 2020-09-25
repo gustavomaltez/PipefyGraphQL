2403bd60f7a35c03a0fcdc890b6b480f0ed76d8b
Gustavo Maltez <gustavo.maltez@hotmail.com>
2020-09-24 22:53:48 -0300

    Code refactoration

--- src/main.js
+++ src/main.js
@@ -1,49 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pipefyBridge = require('./pipefy');
+const handleSimpleRequest = require('./utils')
 
 const app = express();
+
 app.use(bodyParser.json());
 
-const handleSimpleRequest = (req,res,type) => {
+app.post('/account/infos', (req,res) => handleSimpleRequest(req,res,"infos"));
 
-    //Deconstruct access token
-    const { token } = req.body;
-
-    //Verify if exists a token in body request
-    if(!token) res.status(400).json({"error": "No token identified"});
-
-    //Shows an error if type does not exists
-    if(!type) throw new Error("Expected type of request on function handleSimpleRequest()");
-
-    //Create a new pipefy bridge
-    const pipefy = new pipefyBridge(token);
-
-    //Verify if token is valid
-    pipefy.verifyToken().then((isValid) => {
-
-        //Return a error if token is invalid
-        if(!isValid) return res.json({"error": "Invalid token!"});
-
-        //Define the patterns to make request on pipefy api
-        const pattern = {
-            "infos": pipefy.getUserInfos(),
-            "organizations": pipefy.getOrganizations(),
-        }
-
-        //If token is valid, return user infos
-        pattern[type].then((data) => {
-            return res.json(data)
-        })
-    })
-}
-
-app.post('/account/infos', (req,res) => {
-    handleSimpleRequest(req,res,"infos");
-})
-
-app.post('/account/organizations', (req,res) => {
-    handleSimpleRequest(req,res,"organizations");
-})
+app.post('/account/organizations', (req,res) => handleSimpleRequest(req,res,"organizations"));
 
 app.listen(3000);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,34 @@
+const pipefyBridge = require('../pipefy');
 
-const handleSimpleRequest = async () => {
+module.exports = handleSimpleRequest = (req,res,type) => {
 
+    //Deconstruct access token
+    const { token } = req.body;
+
+    //Verify if exists a token in body request
+    if(!token) res.status(400).json({"error": "No token identified"});
+
+    //Shows an error if type does not exists
+    if(!type) throw new Error("Expected type of request on function handleSimpleRequest()");
+
+    //Create a new pipefy bridge
+    const pipefy = new pipefyBridge(token);
+
+    //Verify if token is valid
+    pipefy.verifyToken().then((isValid) => {
+
+        //Return a error if token is invalid
+        if(!isValid) return res.json({"error": "Invalid token!"});
+
+        //Define the patterns to make request on pipefy api
+        const pattern = {
+            "infos": pipefy.getUserInfos(),
+            "organizations": pipefy.getOrganizations(),
+        }
+
+        //If token is valid, return user infos
+        pattern[type].then((data) => {
+            return res.json(data)
+        })
+    })
 }
-
-module.exports = {handleSimpleRequest}
